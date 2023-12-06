fab3797665be2c262355ef1727f0122aba035af9
zenpoxa <zenpoxa@DESKTOP-P4OGIOM>
2023-12-06 18:37:08 +0100

    Animation et fonctions finies sur les cadres des saisons

--- js/saisons.js
+++ js/saisons.js
@@ -14,18 +14,40 @@ function initLenis() {
 
 initLenis()
 
+// CADRES DE PRESENTATION DES SAISON
+
+// chaque cadre
 const cadres = document.querySelectorAll('.cadre_presentation_saison')
 
 cadres.forEach(cadre => {
-  const avisButton = cadre.querySelector('.avis-button')
 
-  const avisText = cadre.querySelector('.avis-text')
+  // get tous les boutons
+  const allButtons = cadre.querySelectorAll('.trigger')
+  const allTexts = cadre.querySelectorAll('.text')
 
-  avisButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    avisText.classList.toggle('active')
+  // fonction sur chaque bouton
+  allButtons.forEach(btn => {
+    
+    btn.addEventListener('click', (e) => {
+      e.preventDefault()
 
-    // XXX.classList.remove('active')
+      // couleur plus foncée sur le bouton cliqué
+      allButtons.forEach(btn => {
+        btn.style.backgroundColor = 'rgb(var(--color-accent-light))'
+      })
+      btn.style.backgroundColor = 'rgb(var(--color-accent-dark))'
+
+      // texte correspondant affiché
+      allTexts.forEach(txt => {
+        if (txt.getAttribute('data-text') == btn.getAttribute('data-button')) {
+          txt.classList.add('active')
+        }
+        else {
+          txt.classList.remove('active')
+        }
+
+      })
+    })
   })
 
 });
