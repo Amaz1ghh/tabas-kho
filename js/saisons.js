2735ee59f45cfede8c129a30dd43c176eefc4533
cebesson <cebesson@etudiant.univ-rennes1.fr>
2023-12-13 12:23:05 +0100

    ...

--- js/saisons.js
+++ js/saisons.js
@@ -1,18 +1,3 @@
-import Lenis from "@studio-freight/lenis"
-
-// SMOOTH SCROLL LENIS
-function initLenis() {
-    const lenis = new Lenis()
-
-    function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-
-    requestAnimationFrame(raf)
-}
-
-initLenis()
 
 // CADRES DE PRESENTATION DES SAISON
 
---
+++ js/slideUp.js
@@ -0,0 +1,68 @@
+import gsap from "gsap";
+import {splitLetter} from "./utils.js";
+
+export class SlideUp {
+  constructor() {
+    this.initializeState()
+    this.loadSlideUpItems()
+  }
+
+  initializeState() {
+    const slideUpItems = document.querySelectorAll("[data-slide-up]")
+    console.log(slideUpItems)
+
+    slideUpItems.forEach(item => {
+      if (item.querySelectorAll('div, span').length > 0) {
+        splitLetter({el: item, lines: true})
+      } else {
+        splitLetter({el: item})
+      }
+      const letters = item.querySelectorAll("[data-letter]")
+
+      gsap.set(letters, {
+        y: 10,
+        opacity: 0,
+      })
+      letters.forEach(letter => {
+      })
+    })
+
+    gsap.set(slideUpItems, {
+      opacity: 0,
+    })
+  }
+
+  loadSlideUpItems() {
+    const slideUpItems = document.querySelectorAll("[data-slide-up]")
+
+    slideUpItems.forEach(item => {
+      const letters = item.querySelectorAll("[data-letter]")
+
+      gsap.to(letters, {
+        scrollTrigger: {
+          trigger: item,
+          start: "top bottom-=80px",
+          toggleActions: "restart play pause reset",
+          markers: true
+        },
+        duration: 0.1,
+        opacity: 1,
+        y: 0,
+        ease: "inOut",
+        stagger: .75 / (letters.length + 1),
+      })
+
+      gsap.to(item, {
+        scrollTrigger: {
+          trigger: item,
+          start: "top bottom-=80px",
+          toggleActions: "restart play pause reset",
+          markers: true
+        },
+        opacity: 1,
+        duration: .5,
+        ease: "power4.inOut",
+      })
+    })
+  }
+}
