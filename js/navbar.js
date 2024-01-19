d205f832172dc66bce79a4be7837b2461850e927
Zenpoxa <zenpoxa@gmail.com>
2024-01-19 11:57:35 +0100

    bonne navbar pour tout le monde mtnt

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,6 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
 
 const hamburgerMenu = document.querySelector('.hamburger-menu')
 const navButtons = document.querySelector('.navbar ul')
